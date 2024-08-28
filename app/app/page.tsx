244cb529eea6123493814f7c19708273f0f750f8
kiv1n <aloha@kiv1n.ru>
2024-08-28 08:15:52 +0300

    Clear code in landing page

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { CompassIcon, ShoppingBasketIcon, StoreIcon } from "lucide-react";
+import { CompassIcon, StoreIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
