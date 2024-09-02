425098401d6e66a74b7149a919a512c822da9cf0
kiv1n <aloha@kiv1n.ru>
2024-09-02 16:05:40 +0300

    Add text to products page

--- app/app/products/page.tsx
+++ app/app/products/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+// TODO: Implement
 export default function ProductsPage() {
-  return <main className="container py-10 lg:px-80">Products...</main>;
+  return (
+    <main className="container py-10 lg:px-80">
+      <div className="space-y-0.5">
+        <h2 className="text-2xl font-bold tracking-tight">Products</h2>
+        <p className="text-muted-foreground">
+          Amazon products that can be bought for crypto
+        </p>
+      </div>
+    </main>
+  );
 }
