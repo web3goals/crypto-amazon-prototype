207afe95423c16280f6eebf1abe23d880c8bef6c
kiv1n <aloha@kiv1n.ru>
2024-09-04 20:48:46 +0300

    Update test indexer

---
+++ test/src/EventHandlers.ts
@@ -0,0 +1,15 @@
+/*
+ * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
+ */
+import { Storefront, Storefront_ProductListed } from "generated";
+
+Storefront.ProductListed.handler(async ({ event, context }) => {
+  const entity: Storefront_ProductListed = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    asin: event.params.asin,
+    seller: event.params.seller,
+    price: event.params.price,
+  };
+
+  context.Storefront_ProductListed.set(entity);
+});
