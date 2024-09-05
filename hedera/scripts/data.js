335384baf351dac3da5ffa355bd20c7202bad197
kiv1n <aloha@kiv1n.ru>
2024-09-05 15:23:05 +0300

    Deploy hedera contracts

--- hedera/scripts/data.js
+++ hedera/scripts/data.js
@@ -1,4 +1,4 @@
 export const data = {
-  usdtId: "0.0.4821483",
-  usdtEvmAddress: "0x00000000000000000000000000000000004991eb",
+  usdtId: "0.0.4821493",
+  usdtEvmAddress: "0x00000000000000000000000000000000004991f5",
 };
